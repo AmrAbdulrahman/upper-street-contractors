633c34d0f9892ee86246282a6e592e9bc34309b1
Ahmed gaafer <ahmeds.gaafer@gmail.com>
2026-06-07 00:37:46 +0300

    feat(page): register WhatWeDoSection on home page sections query

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,4 +1,5 @@
 export { AccreditationList } from "./accreditation-list";
 export { HeroHeroSection } from "./home-hero";
 export { PageSection, type PageSectionData } from "./page-section";
+export { WhatWeDoSection } from "./what-we-do";
 export { WhoWeAreSection } from "./who-we-are";
--- src/components/sections/page-section.tsx
+++ src/components/sections/page-section.tsx
@@ -1,15 +1,18 @@
 import {
   type AccreditationListFragment,
   type HomeHeroSectionFragment,
+  type WhatWeDoSectionFragment,
   type WhoWeAreSectionFragment,
 } from "@/generated/graphql";
 import { AccreditationList } from "./accreditation-list";
 import { HeroHeroSection } from "./home-hero";
+import { WhatWeDoSection } from "./what-we-do";
 import { WhoWeAreSection } from "./who-we-are";
 
 export type PageSectionData =
   | AccreditationListFragment
   | HomeHeroSectionFragment
+  | WhatWeDoSectionFragment
   | WhoWeAreSectionFragment;
 
 export function PageSection({ section }: { section: PageSectionData }) {
@@ -19,6 +22,9 @@ export function PageSection({ section }: { section: PageSectionData }) {
 
     case "WhoWeAreSection":
       return <WhoWeAreSection data={section} />;
+
+    case "WhatWeDoSection":
+      return <WhatWeDoSection data={section} />;
 
     case "AccreditationList":
       return <AccreditationList data={section} />;
