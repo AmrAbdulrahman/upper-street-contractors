ae9c8a4199bff68465bbee701528fdcfbbea0337
Ahmed gaafer <ahmeds.gaafer@gmail.com>
2026-06-07 16:38:55 +0300

    feat(seo): add dynamic robots.txt from SiteMetaConfig

---
+++ src/app/robots.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from "next";
+import { normalizeSiteUrl } from "@/components/metadata/to-metadata";
+import { getSiteMetaConfig } from "@/components/site-meta-config";
+
+export default async function robots(): Promise<MetadataRoute.Robots> {
+  const config = await getSiteMetaConfig();
+  const siteUrl = normalizeSiteUrl(config?.siteUrl);
+  const indexable = config?.indexable !== false;
+
+  if (!indexable) {
+    return {
+      rules: {
+        userAgent: "*",
+        disallow: "/",
+      },
+    };
+  }
+
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${siteUrl}/sitemap.xml`,
+  };
+}
